a84814241989148e1bd6c2c2eb5a00ba0179c343
Hiago 7k <hiago7k@gmail.com>
2026-08-24 19:48:35 -0300

    modificando a stream para todos caps loock

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,20 @@ import java.util.stream.Collectors;
 class Main{
     public static void main(String[] args){
         // LeetCode #1
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5,6);
+//        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5,6);
+//
+//        numeros.stream()
+//                        .filter(n -> n % 2 == 0)
+//                        .sorted()
+//                .forEach(System.out::println);
 
-        numeros.stream()
-                        .filter(n -> n % 2 == 0)
-                        .sorted()
+        // LeetCode #2
+        //2 - Dada a lista de strings abaixo, converta todas para letras maiúsculas e imprima-as.
+        List<String> palavras = Arrays.asList("java", "stream", "lambda");
+
+        palavras.stream()
+                .map(p -> p.toUpperCase())
                 .forEach(System.out::println);
-        
+
     }
 }
