307d8ffc1f4ed45a1b430b70bfcfe2b4d95a396b
Hiago 7k <hiago7k@gmail.com>
2026-08-24 19:46:00 -0300

    achando numeros pares com streams

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Main{
     public static void main(String[] args){
-        System.out.println("Streams");
+        // LeetCode #1
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5,6);
+
+        numeros.stream()
+                        .filter(n -> n % 2 == 0)
+                        .sorted()
+                .forEach(System.out::println);
+        
     }
 }
